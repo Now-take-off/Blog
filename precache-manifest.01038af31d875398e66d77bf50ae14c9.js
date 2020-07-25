dd3de974df724726cde5d19b78f3c841345969ce
王中坤 <wzk13598201388@163.com>
2020-07-25 17:55:40 +0800

    react-init

---
+++ precache-manifest.01038af31d875398e66d77bf50ae14c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e033458829e849ba7657fd7102911c93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73d958b6de83830890c0",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b45aaff03ac9699c404",
+    "url": "./static/js/2.0d7d43cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0d7d43cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d958b6de83830890c0",
+    "url": "./static/js/main.62f81b2d.chunk.js"
+  },
+  {
+    "revision": "208064bae190e6286342",
+    "url": "./static/js/runtime-main.1a80783a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
